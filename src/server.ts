df1f2428b7899284a77f1982b8c4661418e2696d
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-12 00:22:25 +0530

    update

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,9 @@ const MONGO_URI = process.env.MONGO_URI || '';
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    // ✅ Base test route
-  
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => {
     console.error('MongoDB connection error:', err);
   });
+  
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
